fb23044ac34b58458cd7025cd87802f5490fa728
Dwayne R. Crooks <me@dwaynecrooks.com>
2016-04-18 10:16:04 -0400

    Complete Chapter 5

---
+++ ch5/doesItCompile.hs
@@ -0,0 +1,25 @@
+-- 1
+bigNum = (^) 5 $ 10
+-- wahoo = bigNum $ 10
+-- Yes, because we're trying to apply a number to a number
+wahoo = bigNum -- is one possile fix
+
+-- 2
+x = print
+y = print "woohoo!"
+z = x "hello world"
+-- No
+
+-- 3
+a = (+)
+-- b = 5
+c = b 10
+d = c 200
+-- Yes, because b is not a function that takes two args
+b = a -- is one possible fix
+
+-- 4
+a = 12 + b
+b = 10000 * c
+-- Yes, because c is not defined (assuming 3 was not defined in this file)
+c = 1 -- is one possible fix
---
+++ ch5/sing.hs
@@ -0,0 +1,18 @@
+module Sing where
+
+fstString :: [Char] -> [Char]
+fstString x = x ++ " in the rain"
+
+sndString :: [Char] -> [Char]
+sndString x = x ++ " over the rainbow"
+
+-- sing = if (x > y) then fstString x else sndString y
+--   where
+--     x = "Singin"
+--     y = "Somewhere"
+
+-- 2
+sing = if (x < y) then fstString x else sndString y
+  where
+    x = "Singin"
+    y = "Somewhere"
