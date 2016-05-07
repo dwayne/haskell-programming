47440ac3f288f78dba732b2a93e5455901fa90b6
Dwayne R. Crooks <me@dwaynecrooks.com>
2016-05-07 19:52:20 -0400

    Complete Step 3 of the Hangman game
     - Making a puzzle

--- ch13/hangman/src/Main.hs
+++ ch13/hangman/src/Main.hs
@@ -9,7 +9,9 @@ import System.Random (randomRIO)
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  word <- randomWord'
+  let puzzle = freshPuzzle (fmap toLower word)
+  runGame puzzle
 
 type WordList = [String]
 
@@ -39,3 +41,83 @@ randomWord wl = do
 
 randomWord' :: IO String
 randomWord' = gameWords >>= randomWord
+
+data Puzzle = Puzzle String [Maybe Char] [Char]
+-- the word we're trying to guess
+-- the characters we've filled in so far
+-- the letters we've guessed so far
+
+instance Show Puzzle where
+  show (Puzzle _ discovered guessed) =
+    (intersperse ' ' $ fmap renderPuzzleChar discovered)
+    ++ " Guessed so far: " ++ guessed
+
+renderPuzzleChar :: Maybe Char -> Char
+renderPuzzleChar = maybe '_' id
+
+freshPuzzle :: String -> Puzzle
+freshPuzzle word = Puzzle word discovered []
+  where discovered = fmap (const Nothing) word
+
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle word _ _) c = elem c word
+
+alreadyGuessed :: Puzzle -> Char -> Bool
+alreadyGuessed (Puzzle _ _ guessed) c = elem c guessed
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word fillInSoFar s) c =
+  Puzzle word newFilledInSoFar (c : s)
+  where
+    zipper guessed wordChar guessChar =
+      if wordChar == guessed then Just wordChar else guessChar
+
+    newFilledInSoFar =
+      zipWith (zipper c) word fillInSoFar
+
+    -- For e.g. let c = 'h', word = "his", filledInSoFar = [Nothing, Just 'i', Nothing]
+    -- then zipper 'h' 'h' Nothing  = Just 'h'
+    --      zipper 'h' 'i' Just 'i' = Just 'i'
+    --      zipper 'h' 's' Nothing  = Nothing
+
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+  putStrLn $ "Your guess was: " ++ [guess]
+  case (charInWord puzzle guess, alreadyGuessed puzzle guess) of
+    (_, True) -> do
+      putStrLn "You already guessed that character, pick something else!"
+      return puzzle
+
+    (True, _) -> do
+      putStrLn "This character was in the word, filling in the word accordingly."
+      return (fillInCharacter puzzle guess)
+
+    (False, _) -> do
+      putStrLn "This character wasn't in the word, try again."
+      return (fillInCharacter puzzle guess)
+
+gameOver :: Puzzle -> IO ()
+gameOver (Puzzle wordToGuess _ guessed) =
+  if (length guessed) > 7 then
+    do putStrLn "You lose!"
+       putStrLn $ "The word was: " ++ wordToGuess
+       exitSuccess
+  else return ()
+
+gameWin :: Puzzle -> IO ()
+gameWin (Puzzle _ filledInSoFar _) =
+  if all isJust filledInSoFar then
+    do putStrLn "You win!"
+       exitSuccess
+  else return ()
+
+runGame :: Puzzle -> IO ()
+runGame puzzle = forever $ do
+  gameOver puzzle
+  gameWin puzzle
+  putStrLn $ "Current puzzle is: " ++ show puzzle
+  putStr "Guess a letter: "
+  guess <- getLine
+  case guess of
+    [c] -> handleGuess puzzle c >>= runGame
+    _   -> putStrLn "Your guess must be a single character."
