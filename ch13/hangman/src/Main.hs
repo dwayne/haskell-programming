b1e96de25bcdb111309ea5d03639a9d0afabc12c
Dwayne R. Crooks <me@dwaynecrooks.com>
2016-05-07 12:19:46 -0400

    Complete Step 1 and Step 2 of the Hangman game
     - Importing modules
     - Generating a word list

--- ch13/hangman/src/Main.hs
+++ ch13/hangman/src/Main.hs
@@ -1,5 +1,41 @@
 module Main where
 
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.Maybe (isJust)
+import Data.List (intersperse)
+import System.Exit (exitSuccess)
+import System.Random (randomRIO)
+
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+type WordList = [String]
+
+allWords :: IO WordList
+allWords = do
+  dict <- readFile "data/dict.txt"
+  return (lines dict)
+
+minWordLength :: Int
+minWordLength = 5
+
+maxWordLength :: Int
+maxWordLength = 9
+
+gameWords :: IO WordList
+gameWords = do
+  aw <- allWords
+  return $ filter gameLength aw
+  where gameLength w =
+          let l = length (w :: String)
+          in  l > minWordLength && l < maxWordLength
+
+randomWord :: WordList -> IO String
+randomWord wl = do
+  randomIndex <- randomRIO (0, length wl - 1)
+  return $ wl !! randomIndex
+
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
