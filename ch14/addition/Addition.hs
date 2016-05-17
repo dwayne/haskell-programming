1261f09215af9a8caea0a1af93f6391bdbfef398
Dwayne R. Crooks <me@dwaynecrooks.com>
2016-05-17 09:15:46 -0400

    Redo 14.1 to 14.3

--- ch14/addition/Addition.hs
+++ ch14/addition/Addition.hs
@@ -1,13 +1,23 @@
 module Addition where
 
--- import Test.Hspec
-import Test.QuickCheck
+import Test.Hspec
 
+-- Example 1
+-- main :: IO ()
+-- main = hspec $ do
+--   describe "Addition" $ do
+--     it "1 + 1 is greater than 1" $ do
+--       (1 + 1) > 1 `shouldBe` True
+--     it "2 + 2 is equal to 4" $ do
+--       2 + 2 `shouldBe` 4
+
+-- Example 2
 -- dividedBy :: Integral a => a -> a -> (a, a)
 -- dividedBy num denom = go num denom 0
---   where go n d count
---           | n < d = (count, n)
---           | otherwise = go (n - d) d (count + 1)
+--   where
+--     go n d count
+--       | n < d = (count, n)
+--       | otherwise = go (n - d) d (count + 1)
 --
 -- main :: IO ()
 -- main = hspec $ do
@@ -17,33 +27,32 @@ import Test.QuickCheck
 --     it "22 divided by 5 is 4 remainder 2" $ do
 --       dividedBy 22 5 `shouldBe` (4, 2)
 
--- main :: IO ()
--- main = hspec $ do
---   describe "Addition" $ do
---     it "1 + 1 is greater than 1" $ do
---       (1 + 1) > 1 `shouldBe` True
---     it "2 + 2 is equal to 4" $ do
---       2 + 2 `shouldBe` 4
---     it "x + 1 is always greater than x" $ do
---       property $ \x -> x + 1 > (x :: Int)
+-- Short Exercise
+myMult :: (Eq a, Num a) => a -> a -> a
+myMult x y
+  | sy == 0 || sy == 1                 = go x y 0
+  | sy == (-1) && (sx == 0 || sx == 1) = go y x 0
+  | otherwise                          = go ax ay 0
+  where
+    sx = signum x
+    sy = signum y
+    ax = abs x
+    ay = abs y
+    go _ 0 ab = ab
+    go a b ab = go a (b - 1) (a + ab)
 
--- choose :: System.Random.Random a => (a, a) -> Gen a
--- elements :: [a] -> Gen a
-
--- genBool :: Gen Bool
--- genBool = choose (False, True)
---
--- genBool' :: Gen Bool
--- genBool' = elements [False, True]
---
--- genOrdering :: Gen Ordering
--- genOrdering = elements [LT, EQ, GT]
---
--- genChar :: Gen Char
--- genChar = elements ['a'..'z']
-
-prop_additionGreater :: Int -> Bool
-prop_additionGreater x = x + 1 > x
-
-runQc :: IO ()
-runQc = quickCheck prop_additionGreater
+main :: IO ()
+main = hspec $ do
+  describe "Multiplication" $ do
+    it "5 multiplied by 0 is 0" $ do
+      myMult 5 0 `shouldBe` 0
+    it "0 multiplied by 5 is 0" $ do
+      myMult 0 5 `shouldBe` 0
+    it "2 multiplied by 3 is 6" $ do
+      myMult 2 3 `shouldBe` 6
+    it "-2 multiplied by 3 is -6" $ do
+      myMult (-2) 3 `shouldBe` (-6)
+    it "2 multiplied by -3 is -6" $ do
+      myMult 2 (-3) `shouldBe` (-6)
+    it "-2 multiplied by -3 is 6" $ do
+      myMult (-2) (-3) `shouldBe` 6
