7755d79ee7520a45a2dc212e77891badae913b36
Dwayne R. Crooks <me@dwaynecrooks.com>
2017-01-25 17:00:48 -0400

    Update my answers in ch2

---
+++ ch2/learn.hs
@@ -0,0 +1,13 @@
+-- learn.hs
+
+module Learn where
+-- First, we declare the name of our module so
+-- it can be imported by name in a project.
+-- We won't be doing a project of this size
+-- for a while yet.
+
+x = 10 * 5 + y
+
+myResult = x * 5
+
+y = 10
